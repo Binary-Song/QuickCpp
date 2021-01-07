a0542d72f3902c803a58f3aabbf260a25c313c74
Binary-Song <yeh189@126.com>
2021-01-07 22:14:55 +0800

    fix: File recursive rename problem

--- scripts/deinit.ps1
+++ scripts/deinit.ps1
@@ -2,16 +2,33 @@
 param (
     [Parameter()]
     [string]
-    $ProjectName = "Initialized"
+    $ProjectName = "Uninitialized"
 )
-
-Get-ChildItem . -Exclude *.ps1, *.json -Recurse | ForEach-Object { `
-        $new_name = $_.Name.Replace("Initialized", "Uninitialized")
-    if ($new_name -ne $_.Name) {
-        Rename-Item $_ $_.Name.Replace("Initialized", "Uninitialized")
+function Rename-Tree {
+    param (
+        [Parameter()] 
+        [Object]
+        $Directory
+    )
+    Write-Output $Directory.Name
+    # Rename all files in this folder (files in subfolders are not included)
+    Get-ChildItem $Directory -Exclude *.ps1, *.json | ForEach-Object { `
+            $new_name = $_.Name.Replace("Initialized", "$ProjectName")
+        if ($new_name -ne $_.Name) {
+            Rename-Item $_ $new_name
+        }
     }
-} 
-
-Get-ChildItem . -Exclude *.ps1, *.json -Recurse -File | ForEach-Object { `
-    (Get-Content $_).replace("Initialized", "Uninitialized") | Set-Content $_
+    # Call Rename-Tree on all subfolders recursively
+    Get-ChildItem $Directory -Exclude *.ps1, *.json -Directory | ForEach-Object { `
+            Rename-Tree -Directory $_
+    }
 }
+
+Rename-Tree -Directory .
+
+# replace the content of files
+Get-ChildItem . -Exclude *.ps1, *.json -Recurse -File | ForEach-Object { `
+    (Get-Content $_).replace("Initialized", "$ProjectName") | Set-Content $_
+}
+
+Write-Output "Done!"
--- scripts/init.ps1
+++ scripts/init.ps1
@@ -6,14 +6,30 @@ param (
 )
 
 Write-Output "Initializing repository..."
- 
-Get-ChildItem . -Exclude *.ps1, *.json -Recurse | ForEach-Object { `
-        $new_name = $_.Name.Replace("Uninitialized", "$ProjectName")
-    if ($new_name -ne $_.Name) {
-        Rename-Item $_ $_.Name.Replace("Uninitialized", "$ProjectName")
-    }
-} 
 
+function Rename-Tree {
+    param (
+        [Parameter()] 
+        [Object]
+        $Directory
+    )
+    Write-Output $Directory.Name
+    # Rename all files in this folder (files in subfolders are not included)
+    Get-ChildItem $Directory -Exclude *.ps1, *.json | ForEach-Object { `
+            $new_name = $_.Name.Replace("Uninitialized", "$ProjectName")
+        if ($new_name -ne $_.Name) {
+            Rename-Item $_ $new_name
+        }
+    }
+    # Call Rename-Tree on all subfolders recursively
+    Get-ChildItem $Directory -Exclude *.ps1, *.json -Directory | ForEach-Object { `
+            Rename-Tree -Directory $_
+    }
+}
+
+Rename-Tree -Directory .
+
+# replace the content of files
 Get-ChildItem . -Exclude *.ps1, *.json -Recurse -File | ForEach-Object { `
     (Get-Content $_).replace("Uninitialized", "$ProjectName") | Set-Content $_
 }
